938033e927a9b5092a92be9b380776f6a0ad3fc7
Vincent <vcannall@gmail.com>
2023-07-27 21:27:43 -0700

    updated population route to take patient count

--- patients/src/routes/populate.ts
+++ patients/src/routes/populate.ts
@@ -6,12 +6,13 @@ import { faker } from "@faker-js/faker";
 const router = express.Router();
 
 router.get(
-  "/api/patients/populate/",
+  "/api/patients/populate/:count",
   requireAuth,
   async (req: Request, res: Response) => {
+    const patientCount = Number(req.params.count);
     const patients = [];
 
-    for (let i = 0; i < 100; i++) {
+    for (let i = 0; i < patientCount; i++) {
       const addresses = Array.from({ length: faker.number.int(5) }, () =>
         faker.location.streetAddress()
       );
